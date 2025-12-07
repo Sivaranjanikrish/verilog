077de91ec878552f1cb14031a23b02805f5f1e9a
Sivaranjanikrish <shivaranjanikrish4@gmail.com>
2025-12-07 19:08:20 +0530

    Update 29.ripple_carry_adder.v

--- combinational_ckt/dataflow_level/29.ripple_carry_adder.v
+++ combinational_ckt/dataflow_level/29.ripple_carry_adder.v
@@ -1,17 +1,15 @@
-`timescale 1ns/1ps
-module dec4bitdftb;
-	wire[4:0] diff;
-	reg [3:0]A;
-dec4bitdf uut(.A(A),.diff(diff));
-	initial begin
-		 $dumpfile("dec4bitdf.vcd");
-        $dumpvars(0,dec4bitdftb);
-	$monitor("Time=%t|Initial_valve=%b|Final_value=%b",$time,A,diff);
-	A=4'b0000;#10;
-	A=4'b1000;#10;
-	A=4'b1001;#10;
-	A=4'b1111;#10;
-	$finish;
-end
-endmodule
-
+module rca4bitdf(sum,cout,A,B,Cin);
+	output[3:0]sum;
+	output cout;
+	input[3:0] A,B;
+	input Cin;
+	wire c1,c2,c3;
+	assign sum[0]=A[0]^B[0]^Cin;
+	assign c1=(A[0]&B[0])|(B[0]&Cin)|(Cin&A[0]);
+	assign sum[1]=A[1]^B[1]^c1;
+	assign c2=(A[1]&B[1])|(B[1]&c1)|(c1&A[1]);
+	assign sum[2]=A[2]^B[2]^c2;
+	assign c3=(A[2]&B[2])|(B[2]&c2)|(c2&A[2]);
+	assign sum[3]=A[3]^B[3]^c3;
+	assign cout=(A[3]&B[3])|(B[3]&c3)|(c3&A[3]);
+	endmodule
